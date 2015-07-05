9cc3e030feb226efce62f876f5975c3e3998b67f
Ajdin Imsirovic <ajdika.i@gmail.com>
2015-07-05 05:26:38 +0000

    Copy over from blog.html, starting with <section> and ending with .primary
    Starting to copy the design from the static blog.html into the
    portfolio theme's dynamic left-sidebar template (page-sidebar-left.php)
    =======================================================================

--- page-sidebar-left.php
+++ page-sidebar-left.php
@@ -6,9 +6,12 @@
 
 <?php get_header() ?>
 
-<section class="row">
-    <div class="small-12 columns text-center">
-        <div class="leader">
+<section class="two-column row no-max pad">
+  <div class="small-12 columns">
+    <div class="row">
+      <!-- Primary Column -->
+      <div class="small-12 medium-7 medium-offset-1 medium-push-4 columns">
+        <div class="primary">
             <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
             <h1><?php the_title(); ?></h1>
             <p><?php the_content(); ?></p>
