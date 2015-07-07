d8114a5709ccadc2544f8138e3d524ae313bd409
Ajdin Imsirovic <ajdika.i@gmail.com>
2015-07-07 05:22:59 +0000

    Fix the bug on front-page.php
    (1) front-page.php was breaking the site
    (2) the loop was open but never closed - fixed
    (3) get_template_part of content-portfolio was missing - fixed
    ==============================================================

--- front-page.php
+++ front-page.php
@@ -7,10 +7,12 @@
             <h1><?php the_title(); ?></h1>
             <p><?php the_content(); ?></p>
             <p>Test</p>
+            <?php endwhile; endif; ?>
         </div>
     </div>
 </section>
 
+<?php get_template_part('content', 'portfolio'); ?>
 
 
 <?php get_footer() ?>
