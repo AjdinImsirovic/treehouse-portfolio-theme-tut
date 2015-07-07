d87dfb022bf82a3bae6c3077c329ea7750a6084c
Ajdin Imsirovic <ajdika.i@gmail.com>
2015-07-07 04:49:42 +0000

    Fix the single.php
    (1) single.php was broken
    (2) I fixed it by copy-pasting everything from the final theme
    (3) The code is similar, there's not too many tweaks, and
    (4) it would be a waste of time hunting down a typo for 2 hrs
    (5) 'When in doubt, save time' - everything else can be recycled
    =================================================================

--- single.php
+++ single.php
@@ -1,58 +1,56 @@
-<?php get_header() ?>
+<?php get_header(); ?>
 
 <section class="two-column row no-max pad">
-    <div class="small-12 columns">
-        <div class="row">
-            <!-- Primary Column -->
-            <div class="small-12 medium-7 medium-offset-1 medium-push-4 columns">
-                <div class="primary">
-                    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+  <div class="small-12 columns">
+    <div class="row">
+      <!-- Primary Column -->
+      <div class="small-12 medium-7 medium-offset-1 medium-push-4 columns">
+        <div class="primary">
+    
+		    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-                    <article class="post">
-                        <?php if( get_the_post_thumbnail() ) : ?>
-                        <div class="img-container">
-                            <?php the_post_thumbnail( 'large'); ?>
-                        </div>
-                        <?php endif; ?>
+              <article class="post">                
 
-                        <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-                        <?php the_content(); ?>
-                        <?php the comments_template(); ?>
-                        <ul class="post-meta no-bullet">
-                            <li class="author">
+                <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+                <ul class="post-meta no-bullet">
+                  <li class="author">
+	                  <span class="wpt-avatar small">
+	                    <?php echo get_avatar( get_the_author_meta( 'ID' ), 24 ); ?>
+	                  </span>
+	                  by <?php the_author_posts_link(); ?>
+                  </li>
+                  <li class="cat">in <?php the_category( ', ' ); ?></li>
+                  <li class="date">on <?php the_time('F j, Y'); ?></li>
+                </ul>    
+                <?php if( get_the_post_thumbnail() ) : ?>
+                <div class="img-container">
+                  <?php the_post_thumbnail('large'); ?>
+                </div>  
+                <?php endif; ?>   
 
-                                <span class="wpt-avatar small">
-                        <?php echo get_avatar( get_the_author_meta( 'ID' ), 24); ?>
-                      </span> by
-                                <?php the_author_posts_link(); ?>
+                <?php the_content(); ?>
+                <?php comments_template(); ?>
 
-                            </li>
-                            <li class="cat">in
-                                <?php the_category( ', ' ); ?>
-                            </li>
-                            <li class="cat">on
-                                <?php the_time( 'F j, Y' ); ?>
-                            </li>
-                        </ul>
+              </article>
+     
+			
+			<?php endwhile; else : ?>
+			
+			  <p><?php _e( 'Sorry, no posts found.', 'treehouse-portfolio' ); ?></p>
+			
+			<?php endif; ?>
+    
+		</div>
+	  </div>
+	
 
-                    </article>
+    <?php get_sidebar(); ?>
 
-                    <?php endwhile; else : ?>
-                    <p>
-                        <?php _e( 'Sorry, no pages found.' ); ?>
-                    </p>
-                    <?php endif; ?>
-                </div>
-            </div>
-            <!-- Secondary Column -->
-            <div class="small-12 medium-4 medium-pull-8 columns">
-                <div class="secondary">
-                    <div class="module-heading">Sidebar</h2>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+  </div>
 </section>
 
-<?php get_footer() ?>
+
+
+
+
+<?php get_footer(); ?>
